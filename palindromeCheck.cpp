6119e1dcc225a28892d37c0dd1ea9637b8dd2839
rehasayinn <75813468+rehasayinn@users.noreply.github.com>
2022-03-17 19:29:11 +0300

    Update and rename sayin_oguz_reha_palindrome.cpp to palindromeCheck.cpp

--- palindromeCheck.cpp
+++ palindromeCheck.cpp
@@ -33,10 +33,3 @@ int main() {
 
     cout << endl << true;
 }
-
-/*
-In this function, we are checking if the given sentence is a palindrome. To do that we create two variables the check both from the start and the end.
-After we create these two variables, a while loop is created to do the checking. In this while loop, there are two while loops that check if there are
-non-alphabetical characters. After those characters are skipped, then the two characters are compared to each other. If one single difference is seen,
-the function returns false. If not, the function returns true.
-*/
